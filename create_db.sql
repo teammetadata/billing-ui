bcaa1e536c3efe96ff3669ebb4ad395693430d2a
teammetadata <84279406+teammetadata@users.noreply.github.com>
2021-05-22 17:38:24 -0400

    add db creation

---
+++ create_db.sql
@@ -0,0 +1,12 @@
+create database billing-ui;
+
+CREATE TABLE account
+( first_name varchar(250) NOT NULL,
+  last_name varchar(250) NOT NULL,
+  user_name varchar(250) NOT NULL,
+  password varchar(250),
+  email_id varchar(250) NOT NULL,
+  phone_number varchar(250) NOT NULL
+  acct_number varchar(250) NOT NULL
+  payment_id varchar(250) NOT NULL
+);
